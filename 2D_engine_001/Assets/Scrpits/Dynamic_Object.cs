c4719e2a378aa356a21952f4d472faad7387a435
Jack Wiebe <jack.b.wiebe@gmail.com>
2016-01-22 01:25:00 -0500

    Added Initial Engine
    Sorry it took so long, the walking gave me more issues then I had
    hoped..
    -Bug with frame skipping on animation
    -Need to add background, attack

---
+++ 2D_engine_001/Assets/Scrpits/Dynamic_Object.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dynamic_Object : MonoBehaviour {
+
+	public float Speed = 0.1f;
+	public LayerMask Blocking_Layer;
+
+	private BoxCollider2D box_Collider;
+	private Rigidbody2D rB2D;
+	private float inverse_move_time;
+
+
+
+	// Use this for initialization
+	protected virtual void Start () {
+
+		box_Collider = GetComponent<BoxCollider2D> ();
+		rB2D = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ 2D_engine_001/Assets/Scrpits/Player_Move.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Move : MonoBehaviour {
+
+	public float Speed;
+	private string keyPressed;
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+	void Update(){
+		if (Input.GetKey("a"))
+		{
+			keyPressed = "a";
+			//Quaternion rotation = Quaternion.EulerRotation(0,0,90);
+			transform.eulerAngles = new Vector3(0,0,270);
+            //GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.up * Speed * -1);
+            anim.SetTrigger("Walk_Left");
+			//anim.SetTrigger("Walk");
+        }
+		else if (Input.GetKey("d"))
+		{
+			keyPressed = "d";
+			//Quaternion rotation = Quaternion.EulerRotation(0, 0, 270);
+			transform.eulerAngles = new Vector3(0, 0, 90);
+            //GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.up * Speed * -1);
+            anim.SetTrigger("Walk_Right");
+			//anim.SetTrigger("Walk");
+        }
+		else if (Input.GetKey("s"))
+		{
+			keyPressed = "s";
+			//Quaternion rotation = Quaternion.EulerRotation(0, 0, 180);
+			transform.eulerAngles = new Vector3(0, 0, 0);
+            //GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.up * Speed * -1);
+           // anim.SetTrigger("Walk");
+			anim.SetTrigger("Walk_Down");
+        }
+		else if (Input.GetKey("w"))
+		{
+			keyPressed = "w";
+			//Quaternion rotation = Quaternion.EulerRotation(0, 0, 180);
+			transform.eulerAngles = new Vector3(0, 0, 180);
+            //GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.up * Speed * -1);
+            //anim.SetTrigger("Walk");
+			anim.SetTrigger("Walk_Up");
+        }
+        else
+        {
+            keyPressed = "";
+            anim.SetTrigger("Idle");
+        }
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+		if (Input.anyKey) {
+			if ((keyPressed == "s") || (keyPressed == "w") || (keyPressed == "d") || (keyPressed == "a")) {
+				GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.up * Speed * -1);
+			}
+		}      
+	}
+}
