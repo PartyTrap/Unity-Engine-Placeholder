7151fcf7b5184575c02d9f3db20d20c944a90f68
Jack Wiebe <jack.b.wiebe@gmail.com>
2016-01-25 17:10:00 -0500

    Added Attack animation + script
    ^^^

---
+++ 2D_engine_001/Assets/Scrpits/Player_Attack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Attack : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+			
+		anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			anim.SetTrigger ("Attack");
+		}
+	}
+}
--- 2D_engine_001/Assets/Scrpits/Player_Move.cs
+++ 2D_engine_001/Assets/Scrpits/Player_Move.cs
@@ -5,55 +5,53 @@ public class Player_Move : MonoBehaviour {
 
 	public float Speed;
 	private string keyPressed;
-    Animator anim;
+	Animator anim;
+	private 
 	// Use this for initialization
 	void Start () {
-        anim = GetComponent<Animator>();
+		anim = GetComponent<Animator>();
 	}
 
 	void Update(){
 		if (Input.GetKey("a"))
-		{
-			keyPressed = "a";
-			//Quaternion rotation = Quaternion.EulerRotation(0,0,90);
-			transform.eulerAngles = new Vector3(0,0,270);
-            //GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.up * Speed * -1);
-            anim.SetTrigger("Walk_Left");
-			//anim.SetTrigger("Walk");
-        }
-		else if (Input.GetKey("d"))
-		{
-			keyPressed = "d";
-			//Quaternion rotation = Quaternion.EulerRotation(0, 0, 270);
-			transform.eulerAngles = new Vector3(0, 0, 90);
-            //GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.up * Speed * -1);
-            anim.SetTrigger("Walk_Right");
-			//anim.SetTrigger("Walk");
-        }
+			{
+				keyPressed = "a";
+				transform.eulerAngles = new Vector3(0,0,270);
+		       
+		        anim.SetTrigger("Walk_Left");
+				
+		    }
+			else if (Input.GetKey("d"))
+			{
+					keyPressed = "d";
+					
+						transform.eulerAngles = new Vector3(0, 0, 90);
+		        
+			           anim.SetTrigger("Walk_Right");
+				
+			      }
 		else if (Input.GetKey("s"))
 		{
-			keyPressed = "s";
-			//Quaternion rotation = Quaternion.EulerRotation(0, 0, 180);
-			transform.eulerAngles = new Vector3(0, 0, 0);
-            //GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.up * Speed * -1);
-           // anim.SetTrigger("Walk");
-			anim.SetTrigger("Walk_Down");
-        }
-		else if (Input.GetKey("w"))
-		{
-			keyPressed = "w";
-			//Quaternion rotation = Quaternion.EulerRotation(0, 0, 180);
-			transform.eulerAngles = new Vector3(0, 0, 180);
-            //GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.up * Speed * -1);
-            //anim.SetTrigger("Walk");
-			anim.SetTrigger("Walk_Up");
-        }
-        else
-        {
-            keyPressed = "";
-            anim.SetTrigger("Idle");
-        }
-	}
+					keyPressed = "s";
+					
+					transform.eulerAngles = new Vector3(0, 0, 0);
+	         
+					anim.SetTrigger("Walk_Down");
+			 }
+			else if (Input.GetKey("w"))
+			{
+					keyPressed = "w";
+				
+					transform.eulerAngles = new Vector3(0, 0, 180);
+		       
+					anim.SetTrigger("Walk_Up");
+			      }
+		      else
+		     {
+		           keyPressed = "";
+		           anim.SetTrigger("Idle");
+		       }
+			}
 	// Update is called once per frame
 	void FixedUpdate () {
 
@@ -62,6 +60,6 @@ public class Player_Move : MonoBehaviour {
 			if ((keyPressed == "s") || (keyPressed == "w") || (keyPressed == "d") || (keyPressed == "a")) {
 				GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.up * Speed * -1);
 			}
-		}      
+		}
 	}
 }
