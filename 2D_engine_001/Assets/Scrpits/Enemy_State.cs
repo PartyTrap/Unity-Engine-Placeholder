f2feaec80b9089c22c8e72644e3e031b9f804f3f
Jack Wiebe <jack.b.wiebe@gmail.com>
2016-01-28 11:20:02 -0500

    Added State Script to enemy
    enemy can now die

---
+++ 2D_engine_001/Assets/Scrpits/Enemy_State.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_State : MonoBehaviour {
+
+	public int enemyHealth = 100;
+	public int dmg = 25;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D SWORD)
+	{
+		enemyHealth -= dmg;
+
+	}
+	// Update is called once per frame
+	void Update () {
+		if (enemyHealth <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
