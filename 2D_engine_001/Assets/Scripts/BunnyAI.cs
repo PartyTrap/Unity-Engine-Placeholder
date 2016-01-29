903ffd09cfb80a41f3a4448e8ca84d5943a21459
AustinEathorne <austineathorne@gmail.com>
2016-01-29 01:38:21 -0400

    Bunnies!
    -two chasing bunny enemies
    -each translate along the x and y axis respectively until the player
    comes within range
    -while in range the bunnies will chase
    -when out of range, the bunnies will return to their x or y axis
    starting positions and continue with their translations
    -bug with box collider 2D, not recognizing any collision

---
+++ 2D_engine_001/Assets/Scripts/BunnyAI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BunnyAI : MonoBehaviour 
+{
+ 	
+	public float speed;
+	public float chaseSpeed;
+	public float distance;
+	private float xStartPosition;
+	public float viewRange;
+
+	private float playerDistance;
+	public GameObject player;
+
+	void Start () 
+	{
+		xStartPosition = transform.position.x;
+	}
+	void FixedUpdate ()
+	{
+
+		playerDistance = Vector2.Distance (player.transform.position, transform.position);
+		//Rigidbody2D.constraints.FreezePosition;
+
+		if (playerDistance <= viewRange)
+		{
+			transform.LookAt(player.transform.position);
+			transform.position += transform.forward * chaseSpeed * Time.deltaTime;
+
+			transform.eulerAngles = new Vector3 (0f, 0f, transform.eulerAngles.z);
+		}
+			
+		else if (playerDistance > viewRange)
+		{
+			if ((speed < 0 && transform.position.x < xStartPosition) || (speed > 0 && transform.position.x > xStartPosition + distance))
+			{
+				speed *= -1;
+			}
+			transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+		}	
+	}
+}
---
+++ 2D_engine_001/Assets/Scripts/BunnyAIy.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BunnyAIy : MonoBehaviour
+{
+
+	public float speed;
+	public float chaseSpeed;
+	public float distance;
+	private float yStartPosition;
+	public float viewRange;
+
+	private float playerDistance;
+	public GameObject player;
+
+	void Start () 
+	{
+		yStartPosition = transform.position.y;
+	}
+	void FixedUpdate ()
+	{
+
+		playerDistance = Vector2.Distance (player.transform.position, transform.position);
+		//Rigidbody2D.constraints.FreezePosition;
+
+		if (playerDistance <= viewRange)
+		{
+			transform.LookAt(player.transform.position);
+			transform.position += transform.forward * chaseSpeed * Time.deltaTime;
+
+			transform.eulerAngles = new Vector3 (0f, 0f, transform.eulerAngles.z);
+		}
+
+		else if (playerDistance > viewRange)
+		{
+			if ((speed < 0 && transform.position.y < yStartPosition) || (speed > 0 && transform.position.y > yStartPosition + distance))
+			{
+				speed *= -1;
+			}
+			transform.position = new Vector2(transform.position.x, transform.position.y  + speed * Time.deltaTime);
+		}	
+	}
+}
