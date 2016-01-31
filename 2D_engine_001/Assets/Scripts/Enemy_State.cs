a43741dcae913816b9e06eac621b1b0092136d94
Jack Wiebe <jack.b.wiebe@gmail.com>
2016-01-31 17:24:57 -0500

    Added Knockback on none stationary enemies
    knockback can be tweaked through each enemies state script

--- 2D_engine_001/Assets/Scripts/Enemy_State.cs
+++ 2D_engine_001/Assets/Scripts/Enemy_State.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class Enemy_State : MonoBehaviour {
 
+	//public GameObject enemy;
+	//public Transform enemyPos;
 	public int enemyHealth = 100;
 	public int dmg = 25;
+	public float knockBack = 100.0f;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,6 +16,8 @@ public class Enemy_State : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D SWORD)
 	{
 		enemyHealth -= dmg;
+		//this.GetComponent<BunnyAI> ().enabled = false;
+		this.GetComponent<Rigidbody2D> ().AddForce (transform.up * knockBack * -1 );
 
 	}
 	// Update is called once per frame
