e1813b47c82420019d398540169223c4becabbca
R-McBride <rmcbride@C416IT38.SCT.local>
2016-02-02 15:30:14 -0500

    Added Map

--- 2D_engine_001/Assets/Scripts/mapGeneration.cs
+++ 2D_engine_001/Assets/Scripts/mapGeneration.cs
@@ -9,24 +9,29 @@ public class mapGeneration : MonoBehaviour {
 	public int mapWidth = 50;
 	public GameObject treeTile;
 	public GameObject grassTile;
-
+	private int[,] map;
 
 
 	// Use this for initialization
 	void Start () {
 
+		map = Level1.map;
+
 		//Put things in the map
-		for (int x = 0; x < mapWidth; x++) {
-			for (int y = 0; y < mapHeight; y++) {
+		for (int y = 0; y < mapWidth; y++) {
+			for (int x = 0; x < mapHeight; x++) {
 				//Make everything grass
-				GameObject gTile = Instantiate (grassTile) as GameObject;
-				gTile.transform.parent = gameObject.transform;
-				gTile.transform.localPosition = new Vector3 (x, y, 0);
+
+				if (map[y,x] == 0) {
+					GameObject gTile = Instantiate (grassTile) as GameObject;
+					gTile.transform.parent = gameObject.transform;
+					gTile.transform.localPosition = new Vector3 (x, -y, 0);
+				}
 				//Make trees at the borders
-				if (x == 0 || x == mapWidth - 1 || y == 0 || y == mapHeight - 1) {
+				if (map[y,x] == 1) {
 					GameObject tTile = Instantiate (treeTile) as GameObject;
 					tTile.transform.parent = gameObject.transform;
-					tTile.transform.localPosition = new Vector3 (x, y, 0);
+					tTile.transform.localPosition = new Vector3 (x, -y, 0);
 				}
 
 			}
