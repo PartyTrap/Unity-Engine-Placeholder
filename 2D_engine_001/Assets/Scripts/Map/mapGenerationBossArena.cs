908c6a78fd93cfc57850d7af9cee6125e8cf998e
timfernandoGH <timothy_fern@live.com>
2016-02-07 12:24:48 +0900

    Boss Arena Map Generation
    Made a simple square map for the boss battle using the map generator

---
+++ 2D_engine_001/Assets/Scripts/Map/mapGenerationBossArena.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mapGenerationBossArena : MonoBehaviour {
+
+	[SerializeField]
+	public int mapHeight = 50;
+	public int mapWidth = 50;
+	public GameObject treeTile;
+	public GameObject grassTile;
+	private int[,] map;
+
+
+	// Use this for initialization
+	void Start () {
+
+		map = BossArena.map;
+
+		//Put things in the map
+		for (int y = 0; y < mapWidth; y++) {
+			for (int x = 0; x < mapHeight; x++) {
+				//Make everything grass
+
+				if (map[y,x] == 0) {
+					GameObject gTile = Instantiate (grassTile) as GameObject;
+					gTile.transform.parent = gameObject.transform;
+					gTile.transform.localPosition = new Vector3 (x, -y, 0);
+				}
+				//Make trees at the borders
+				if (map[y,x] == 1) {
+					GameObject tTile = Instantiate (treeTile) as GameObject;
+					tTile.transform.parent = gameObject.transform;
+					tTile.transform.localPosition = new Vector3 (x, -y, 0);
+				}
+
+			}
+		}
+
+
+
+	}
+}
