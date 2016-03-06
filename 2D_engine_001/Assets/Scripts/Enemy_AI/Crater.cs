193c605a6c6d6f784bf63979079d9838c613d9a7
timfernandoGH <timothy_fern@live.com>
2016-03-07 00:00:51 +0900

    Complete Boss Battle
    All 3 attacks done
    Edited Enemy_State to accomodate for boss enemy types

---
+++ 2D_engine_001/Assets/Scripts/Enemy_AI/Crater.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crater : MonoBehaviour {
+	public int counter = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter++;
+		if (counter == 160) {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ 2D_engine_001/Assets/Scripts/Gameplay/ThornHit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThornHit : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D c){
+		if (c.gameObject.tag == "Player") {
+			c.gameObject.GetComponent<Player_State> ().playerHealth -= 10;
+		}
+		Destroy (this.gameObject);
+	}
+}
