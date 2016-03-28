1da9877b011759caa5900be1342c8300fe40d9b9
Jack Wiebe <jack.b.wiebe@gmail.com>
2016-03-28 12:58:58 -0400

    Small Boss Fix

--- 2D_engine_001/Assets/Scripts/Enemy_AI/SpiderBoss/HomingMissileCollision.cs
+++ 2D_engine_001/Assets/Scripts/Enemy_AI/SpiderBoss/HomingMissileCollision.cs
@@ -2,8 +2,26 @@
 using System.Collections;
 
 public class HomingMissileCollision : MonoBehaviour {
-	public GameObject missile;
+    public Player_State PS;
+    public float dmg;
+
+    void Start ()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        PS = player.GetComponent <Player_State> ();
+        Physics2D.IgnoreLayerCollision(11, 10);
+        Physics2D.IgnoreLayerCollision(9, 11);
+
+    }
+
 	void OnCollisionEnter2D(Collision2D c){
-		Destroy (missile);
+
+        if (c.gameObject.tag == "Player")
+        {
+            PS.playerHealth -= (dmg - PS.resistance);
+            Destroy(this.gameObject);
+        }
+       
 	}
 }
