f58a6d6cd647be11c937dd04f7e997b7650fc231
NeilHansen <neilmickneilh12@hotmail.com>
2016-03-28 14:01:13 -0400

    Added Enemy GUI

---
+++ 2D_engine_001/Assets/EnemyGUIManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGUIManager : MonoBehaviour {
+
+	[SerializeField]private Enemy_State es;
+	private float dmg;
+	private float currHealth;
+	private float move;
+
+	// Use this for initialization
+	void Start () {
+		currHealth = es.enemyHealth;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		dmg = currHealth - es.enemyHealth;
+		GetComponent<RectTransform> ().Translate (new Vector3 (-dmg, 0, 0));
+	}
+}
