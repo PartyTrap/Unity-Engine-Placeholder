455fa326a0bf04178ddaf73473aa995f3b9421c4
Austin Eathorne <austineathorne@gmail.com>
2016-04-03 15:10:30 -0500

    Credits
    Together but not connected, going to add more to it as well but this is
    the base for now. Also I'm open to change titles etc... :+1:

--- 2D_engine_001/Assets/Scripts/Enemy_AI/deathWebDestroy.cs
+++ 2D_engine_001/Assets/Scripts/Enemy_AI/deathWebDestroy.cs
@@ -13,6 +13,6 @@ public class deathWebDestroy : MonoBehaviour {
 	{
 		yield return new WaitForSeconds(5.0f);
 		Destroy(this.gameObject);
-		Debug.Log ("deathWeb should go away");
+		//Debug.Log ("deathWeb should go away");
 	}
 }
