8d1ea42a13f834e77e93f6adbcf9ff1b96ca5467
Jack Wiebe <jack.b.wiebe@gmail.com>
2016-04-04 00:55:03 -0400

    Added Player Data - 1st iteration using playerprefs

--- 2D_engine_001/Assets/Scripts/Data/SceneLoader.cs
+++ 2D_engine_001/Assets/Scripts/Data/SceneLoader.cs
@@ -3,9 +3,18 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
+
+    [SerializeField] private GameObject audioManager;
+
 	public void LoadToScene (string sceneName){
 		SceneManager.LoadScene(sceneName);
 	}
+
+    public void LoadFromMem (){
+        Instantiate(audioManager);
+        Debug.Break();
+        SceneManager.LoadScene(PlayerData.Instance.Level.ToString());
+    }
 
 	}
 	
---
+++ 2D_engine_001/Assets/Scripts/Player/Player_Data.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerData 
+{
+    // C# Class Singleton:
+
+    private static PlayerData instance = null;
+
+    public static PlayerData Instance
+    {
+        get {
+            if (instance == null)
+            {
+                instance = new PlayerData();
+            }
+            return instance;
+        }
+    }
+
+
+    // PlayerData Constructor:
+    private PlayerData ()
+    {
+       Level = PlayerPrefs.GetString("Level");
+       Health = PlayerPrefs.GetInt("Health");
+    }
+
+
+    // Accessors:
+
+    private string level;// = "Ranged_Intro";
+    public string Level{
+        get{ return this.level;}
+        set{
+            level = value;
+            PlayerPrefs.SetString("Level", level);
+        }
+    }
+
+    private int health = 0;
+
+    public int Health
+    {
+        get {
+            return this.health;
+        }
+        set {
+            health = value;
+
+            PlayerPrefs.SetInt("Health", health);
+          
+            }
+    }
+        
+}
