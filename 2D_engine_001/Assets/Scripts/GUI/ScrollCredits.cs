6730b9668ee9140abdd0eb7c61365f1c33bb85a1
Austin Eathorne <austineathorne@gmail.com>
2016-04-03 20:23:34 -0500

    credits
    fade animation for 'thanks for playing'

--- 2D_engine_001/Assets/Scripts/GUI/ScrollCredits.cs
+++ 2D_engine_001/Assets/Scripts/GUI/ScrollCredits.cs
@@ -71,8 +71,7 @@ public class ScrollCredits : MonoBehaviour {
 
 	IEnumerator LoadScene()
 	{
-		
-		yield return new WaitForSeconds (3.0f);
+		yield return new WaitForSeconds (5.0f);
 		SceneManager.LoadScene (scene);
 	}
 
